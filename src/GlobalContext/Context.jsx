b6355f11b1c7bc3709c652f46b49873b23d347cc
Zubair Ahmed <zubairahmed@Zubairs-MacBook-Pro-2.local>
2024-01-19 15:08:58 -0800

    testing

--- src/GlobalContext/Context.jsx
+++ src/GlobalContext/Context.jsx
@@ -53,6 +53,18 @@ const GlobalProvider = ({ children }) => {
             const response = await axios(`https://stl-api.efmt.app/f/api/looks/${id}`)
             if (response.status === 200) {
                 dispatch(getProductSuccess(response.data))
+                let config = {
+                    method: 'get',
+                    maxBodyLength: Infinity,
+                    url:`https://www.efavormart.com/search?q=${response.data.shop_look.products.map(obj => `id:${obj.product_id}`).join(' OR ')}&type=product&view=tw-shop-the-look`,
+                    credentials: 'same-origin',
+                    // headers: { 
+                    //   'Cookie': '_cmp_a=%7B%22purposes%22%3A%7B%22a%22%3Atrue%2C%22p%22%3Atrue%2C%22m%22%3Atrue%2C%22t%22%3Atrue%7D%2C%22display_banner%22%3Afalse%2C%22sale_of_data_region%22%3Afalse%7D; _landing_page=%2Fsearch%3Fq%3Did%3A6890497081390%2520OR%2520id%3A6782896570414%2520OR%2520id%3A6783481872430%2520OR%2520id%3A4715200315438%2520OR%2520id%3A6707317473326%2520OR%2520id%3A6810000261166%26type%3Dproduct%26view%3Dtw-shop-the-look; _orig_referrer=; _shopify_s=d7c4a3cb-d736-4523-8d97-54efade523e0; _shopify_y=fa497a56-3ecd-4ca0-affe-92f5e31702ed; cart_currency=USD; localization=US; secure_customer_sig=',
+                    // }
+                  };
+
+                const resp=await axios(config)
+                console.log(resp.data);
             }
             else {
                 throw response
